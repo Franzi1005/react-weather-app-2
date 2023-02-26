9ca5de07d05fc49d66c9c44baf4182c44198f7d0
Franzi1005 <fschallhorn@gmail.com>
2023-02-26 14:23:20 +0100

    added geo location functionality and API for openWeather

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
+
 export default function Weather() {
+  let now = new Date();
+
+  let allMonths = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let date = now.getDate();
+  let month = allMonths[now.getMonth()];
+  let year = now.getFullYear();
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let weekday = weekdays[now.getDay()];
+
+  let fullDate = `${weekday}, ${month} ${date}, ${hours}:${minutes}`;
+
+  const [city, setCity] = useState(" ");
+  const [temperature, setTemperature] = useState(" ");
+  const [wind, setWind] = useState(" ");
+  const [description, setDescription] = useState(" ");
+  const [humidity, setHumidity] = useState(" ");
+  const [icon, setIcon] = useState(" ");
+  const [message, setMessage] = useState("");
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let apiKey = "8a869017a9bbe9c440c0fea9e1fa0af6";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  function showWeather(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setWind(response.data.wind.speed);
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setIcon(response.data.weather.icon);
+    setMessage(response.data.name);
+  }
+
+  function handleSearch(event) {
+    event.preventDefault();
+    axios.get(url).then(showWeather);
+  }
+
+  function showLocalTemp() {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+
+  function showPosition(position) {
+    let lon = position.coords.longitude;
+    let lat = position.coords.latitude;
+    let apiKey = "8a869017a9bbe9c440c0fea9e1fa0af6";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(showWeather);
+  }
   return (
     <div className="Weather">
       <div className="weather-section">
@@ -23,32 +108,45 @@ export default function Weather() {
             type="text"
             className="form-control"
             placeholder="Type a city..."
+            onChange={updateCity}
           ></input>
-          <button type="submit" className="btn btn-primary">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSearch}
+          >
             Search
           </button>
-          <button type="button" className="btn btn-success">
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={showLocalTemp}
+          >
             Current
           </button>
         </form>
-        <h1>Sydney</h1>
-        <p>Saturday, 25.02.2023, 13:38 </p>
+        <h1>{message}</h1>
+        <p>{fullDate} </p>
         <div className="container">
           <div className="row">
             <div className="col">
               <ul className="weather-data">
-                <li>Clouds</li>
-                <li>Wind: 3 km/h</li>
-                <li>Precipitation: 76%</li>
+                <li>{description}</li>
+                <li>Wind: {wind} km/h</li>
+                <li>Humidity: {humidity}%</li>
               </ul>
             </div>
             <div className="col">
-              <img src="rain.png" alt="Weather Condition" />
-              <h2>2°C</h2>
+              {icon}
+              <h2>{temperature}°C</h2>
             </div>
           </div>
         </div>
       </div>
+      <a href="https://github.com/Franzi1005/react-weather-app-2">
+        Open-source code
+      </a>{" "}
+      by Franziska Schallhorn
     </div>
   );
 }
